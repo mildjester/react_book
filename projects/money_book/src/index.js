d2c5b6076ccf68125c01b6a3b313783478cf879b
mildjester <16119464+mildjester@users.noreply.github.com>
2018-03-11 23:50:09 +0900

    learn 5-2-7

--- projects/money_book/src/index.js
+++ projects/money_book/src/index.js
@@ -12,7 +12,7 @@ const MoneyBook = () => {
 
     return (
         <div>
-            <h1>小遣い帳</h1>
+            <Title>小遣い帳</Title>
             <table className="book">
                 <thead>
                     <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
@@ -38,9 +38,15 @@ const MoneyBookItem = (props) => {
         </tr>
     )
 }
-
 MoneyBookItem.propTypes = {
     book: PropTypes.object.isRequired
+}
+
+const Title = (props) => {
+    return (<h1>{props.children}</h1>)
+}
+Title.propTypes = {
+    children: PropTypes.string
 }
 
 ReactDom.render(
